6c8aa76b81ca89d07fe5b738cb8164e6e9bf7831
alireza <alirezasoheili1224@yahoo.com>
2023-04-30 14:39:13 +0330

    implement login

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -14,8 +14,9 @@ export class AuthController {
     }
 
     @Post('/signin')
-    signin() { 
-        this.authService.signin()
+    signin(@Body() dto: AuthDto) { 
+        return this.authService.signin(dto)
+
      }
 
     @Post('/logout')
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt'
 import { Tokens } from './types';
@@ -7,9 +7,9 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService, private jwt: JwtService) {}
-    
-    async signup(dto: AuthDto) {
+    constructor(private prisma: PrismaService, private jwt: JwtService) { }
+
+    async signup(dto: AuthDto): Promise<Tokens> {
         const hash = await this.hashData(dto.password)
 
         const newUser = await this.prisma.user.create({
@@ -19,19 +19,36 @@ export class AuthService {
             }
         })
 
-        const { accessToken, refreshToken } = await this.getTokens(newUser.id, newUser.email)
+        const tokens = await this.getTokens(newUser.id, newUser.email)
 
-        await this.updateRefreshTokenHash(newUser.id, refreshToken)
-        return { refreshToken, accessToken }
+        await this.updateRefreshTokenHash(newUser.id, tokens.refreshToken)
+
+        return tokens 
     }
-    
-    async signin() {
+
+    async signin(dto: AuthDto): Promise<Tokens> {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        })
+
+        if(!user) throw new ForbiddenException('Invalid Credentials')
+
+        const isPasswordValid = await bcrypt.compare(dto.password, user.hash)
+
+        if(!isPasswordValid) throw new ForbiddenException('Invalid Credentials') 
+
+        const tokens = await this.getTokens(user.id, user.email)
+        await this.updateRefreshTokenHash(user.id, tokens.refreshToken)
+
+        return tokens 
     }
-    logout() {}
-    refreshToken() {}
+    logout() { }
+    refreshToken() { }
 
 
-    async updateRefreshTokenHash(userId: number, refreshToken: string){
+    async updateRefreshTokenHash(userId: number, refreshToken: string) {
         const hash = await this.hashData(refreshToken)
         await this.prisma.user.update({
             where: {
@@ -43,10 +60,10 @@ export class AuthService {
         })
     }
 
-    async getTokens(userId: number, email: string) :Promise<{accessToken: string, refreshToken: string}>{
+    async getTokens(userId: number, email: string): Promise<{ accessToken: string, refreshToken: string }> {
         const accessTokenPromise = this.jwt.signAsync({ sub: userId, email }, {
             secret: "120SJDHue9jiosdaml;1asdl;asdpeokSAJKLDjKAd",
-            expiresIn: 60 * 15 
+            expiresIn: 60 * 15
         })
 
         const refreshTokenPromise = this.jwt.signAsync({ sub: userId, email }, {
@@ -56,10 +73,10 @@ export class AuthService {
 
         const [accessToken, refreshToken] = await Promise.all([accessTokenPromise, refreshTokenPromise])
 
-        return {accessToken, refreshToken}
+        return { accessToken, refreshToken }
     }
 
-    hashData(data: string){
+    hashData(data: string) {
         return bcrypt.hash(data, 10)
     }
 }
